45d7552ef142343a6dd62f83ab8ddacebe47dfe1
Tamas Banka <bankatamas.hu@gmail.com>
2021-08-27 09:46:38 +0200

    Extension project added

---
+++ WebSharper.SignalR/Main.fs
@@ -0,0 +1,23 @@
+﻿namespace WebSharper.SignalR
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.InterfaceGenerator
+
+module Definition =
+
+    let Assembly =
+        Assembly [
+            Namespace "WebSharper.SignalR.Resources" [
+                Resource "SignalRCDN" "https://cdn.jsdelivr.net/npm/signalr@2.4.2/jquery.signalR.min.js"
+            ]
+        ]
+
+[<Sealed>]
+type Extension() =
+    interface IExtension with
+        member ext.Assembly =
+            Definition.Assembly
+
+[<assembly: Extension(typeof<Extension>)>]
+do ()
