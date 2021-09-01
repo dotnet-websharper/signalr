18153fbc00517aa177b20a06d75058cee50e6251
jooseppi12 <urijozsef@europe.com>
2021-09-01 11:54:58 +0200

    Use paket, update to ws5 tooling, prepare for GHA

---
+++ build.fsx
@@ -0,0 +1,37 @@
+#if INTERACTIVE
+#r "nuget: FAKE.Core"
+#r "nuget: Fake.Core.Target"
+#r "nuget: Fake.IO.FileSystem"
+#r "nuget: Fake.Tools.Git"
+#r "nuget: Fake.DotNet.Cli"
+#r "nuget: Fake.DotNet.AssemblyInfoFile"
+#r "nuget: Fake.DotNet.Paket"
+#r "nuget: Paket.Core"
+#else
+#r "paket:
+nuget FAKE.Core
+nuget Fake.Core.Target
+nuget Fake.IO.FileSystem
+nuget Fake.Tools.Git
+nuget Fake.DotNet.Cli
+nuget Fake.DotNet.AssemblyInfoFile
+nuget Fake.DotNet.Paket
+nuget Paket.Core //"
+#endif
+
+#load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
+#r "System.Xml.Linq"
+
+// Only reference these packages from editors/non fake-cli tools
+#if !FAKE
+    // To have proper language service in the editor
+    #r "netstandard"
+    // To help FAKE related IntelliSense in editor
+    #load "./.fake/build.fsx/intellisense_lazy.fsx"
+#endif
+
+open WebSharper.Fake
+
+LazyVersionFrom "WebSharper" |> WSTargets.Default
+|> MakeTargets
+|> RunTargets
